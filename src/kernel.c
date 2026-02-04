5e794fd397cc52da7ec8f0f6d352100faea66657
nattensleplubo <nathan.gobert2002@gmail.com>
2026-02-04 18:10:25 +0100

    jeeeej

--- src/kernel.c
+++ src/kernel.c
@@ -6,6 +6,9 @@
 # define	VGA_WIDTH	80
 # define	VGA_HEIGHT  25
 
+# define	TAB_COUNT		9
+# define	SCREEN_CELLS	(VGA_WIDTH * VGA_HEIGHT)
+
 // CONSTANTS
 size_t		terminal_row;
 size_t		terminal_column;
@@ -32,6 +35,16 @@ enum vga_color {
 	VGA_COLOR_LIGHT_BROWN = 14,
 	VGA_COLOR_WHITE = 15,
 };
+
+typedef struct	s_tab {
+	uint16_t	buffer[SCREEN_CELLS];
+	size_t		row;
+	size_t		col;
+	uint8_t		color;
+} t_tab;
+
+static t_tab	tabs[TAB_COUNT];
+static int		current_tab = 0;
 
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
 {
@@ -67,7 +80,7 @@ void	terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 	terminal_buffer[i] = vga_entry(c, color);
 }
 
-void terminal_putchar(char c) 
+void	terminal_putchar(char c) 
 {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
@@ -77,20 +90,46 @@ void terminal_putchar(char c)
 	}
 }
 
-void terminal_write(const char* data, size_t size) 
+void	terminal_write(const char* data, size_t size) 
 {
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
 
-void terminal_writestring(const char* data) 
+void	terminal_writestring(const char* data) 
 {
 	terminal_write(data, k_strlen(data));
+}
+
+void	terminal_clear(void) {
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
+		}
+	}
+	terminal_row = 0;
+	terminal_column = 0;
+}
+
+void	terminal_setpos(size_t x, size_t y) {
+	terminal_column = x;
+	terminal_row = y;
+}
+
+void	terminal_writestring_at(const char *str, size_t x, size_t y) {
+	terminal_setpos(x, y);
+	terminal_writestring(str);
+}
+
+static void delay(void)
+{
+	for (volatile uint32_t i = 0; i < 30000000; i++)
+		__asm__ volatile("nop");
 }
 
 ////////// MAIN //////////
 void	kernel_main(void) {
 	terminal_init();
-	terminal_writestring("42");
-	while(1);
+	terminal_writestring("Hello, kernel World!\n");
 }
