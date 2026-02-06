ec98183e77536c3c8030b70a2177afcc54c8fccf
nattensleplubo <nathan.gobert2002@gmail.com>
2026-02-06 15:41:15 +0100

    Added keyboard support without setting up an IDT

--- src/kernel.c
+++ src/kernel.c
@@ -9,6 +9,9 @@
 # define	TAB_COUNT		9
 # define	SCREEN_CELLS	(VGA_WIDTH * VGA_HEIGHT)
 
+# define	KEYBOARD_DATA_PORT		0x60
+# define	KEYBOARD_STATUS_PORT	0x64
+
 // CONSTANTS
 size_t		terminal_row;
 size_t		terminal_column;
@@ -36,6 +39,45 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
 
+    unsigned char keyboard_map[128] = {
+        0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+        '9', '0', '-', '=', '\b',	/* Backspace */
+        '\t',			/* Tab */
+        'q', 'w', 'e', 'r',	/* 19 */
+        't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+        0,			/* 29   - Control */
+        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+        '\'', '`',   0,		/* Left shift */
+        '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+        'm', ',', '.', '/',   0,				/* Right shift */
+        '*',
+        0,	/* Alt */
+        ' ',	/* Space bar */
+        0,	/* Caps lock */
+        -1,	/* 59 - F1 key ... > */
+        -2,   -3,   -4,   -5,   -6,   -7,   -8,   -9,
+        0,	/* < ... F10 */
+        0,	/* 69 - Num lock*/
+        0,	/* Scroll Lock */
+        0,	/* Home key */
+        0,	/* Up Arrow */
+        0,	/* Page Up */
+        '-',
+        0,	/* Left Arrow */
+        0,
+        0,	/* Right Arrow */
+        '+',
+        0,	/* 79 - End key*/
+        0,	/* Down Arrow */
+        0,	/* Page Down */
+        0,	/* Insert Key */
+        0,	/* Delete Key */
+        0,   0,   0,
+        0,	/* F11 Key */
+        0,	/* F12 Key */
+        0,	/* All other keys are undefined */
+    };
+
 typedef struct	s_tab {
 	uint16_t	buffer[SCREEN_CELLS];
 	size_t		row;
@@ -57,11 +99,36 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 }
 
 
+//FOR KEYBOARD ENTRY
+static inline uint8_t	inb(uint16_t port) {
+	uint8_t	result;
+	__asm__ volatile("in %1, %0" : "=a"(result) : "Nd"(port));
+	return result;
+}
+
+static inline int	keyboard_has_data(void) {
+	return inb(KEYBOARD_STATUS_PORT) & 0x01;
+}
+
+uint8_t	read_keyboard(void) {
+	return inb(KEYBOARD_DATA_PORT);
+}
+
+void	handle_keyboard(void) {
+	if (keyboard_has_data()) {
+		uint8_t	scancode = read_keyboard();
+		if (!(scancode & 0x80)) {
+			terminal_putchar(keyboard_map[scancode]);
+		}
+	}
+}
+
+
 // INIT
 void	terminal_init(void) {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
 
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -132,5 +199,7 @@ static void delay(void)
 void	kernel_main(void) {
 	terminal_init();
 	terminal_writestring("Hello, kernel World!\n");
-	// while (1);
+	while (1) {
+		handle_keyboard();
+	}
 }
